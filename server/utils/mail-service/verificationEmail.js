58af102ce5cc3a1fa0d13127d615ec42fded4ac8
Aziz Rbaii <61419223+azizrbii90@users.noreply.github.com>
2022-11-30 19:02:21 +0100

    fix security prb

--- server/utils/mail-service/verificationEmail.js
+++ server/utils/mail-service/verificationEmail.js
@@ -9,8 +9,8 @@ module.exports = ({ email, username, buttonLink, type }) => {
             port: 465,
             secure: true,
             auth: {
-                user: "rbiiaziz900@gmail.com",
-                pass: "vakiqzlkgnqgirsx"
+                user: "email",
+                pass: "mot de passe"
             }
         })
 
